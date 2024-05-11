8ff3515e8e8f35936b925b45579b00f6f9f460df
Nana <160034837+a-aaangeeel@users.noreply.github.com>
2024-05-11 18:10:39 +0300

    train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,38 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+TrainNew::TrainNew() {
+    firstCage = nullptr;
+    stepsCount = 0;
+}
+
+void TrainNew::addCage(bool bulbState) {
+    CageNew* newCage = new CageNew;
+    newCage->bulbOn = bulbState;
+    if (firstCage == nullptr) {
+        firstCage = newCage;
+        firstCage->nextCage = firstCage;
+        firstCage->prevCage = firstCage;
+    } else {
+        CageNew* lastCage = firstCage->prevCage;
+        lastCage->nextCage = newCage;
+        newCage->prevCage = lastCage;
+        newCage->nextCage = firstCage;
+        firstCage->prevCage = newCage;
+    }
+    stepsCount++;
+}
+
+int TrainNew::getTrainLength() {
+    if (firstCage == nullptr) return 0;
+    int length = 0;
+    CageNew* currentCage = firstCage;
+    do {
+        length++;
+        currentCage = currentCage->nextCage;
+    } while (currentCage != firstCage);
+    return length;
+}
+
+int TrainNew::getStepsCount() {
+    return stepsCount;
+}
