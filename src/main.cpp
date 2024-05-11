00de568c9dcfe519c3418f8f0bdaf32e0dc9479c
Nana <160034837+a-aaangeeel@users.noreply.github.com>
2024-05-11 18:45:51 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,13 @@
 #include "train.h"
 
 int main() {
- Train train;
-  int count = 60; 
-  
+  Train train;
+  int count = 60;
+
   while (count--)
     train.addCage(false);
 
   std::cout << train.getLength() << std::endl;
   std::cout << train.getOpCount() << std::endl;
-  
-  TrainNew newTrain;
-  count = 60;
-
-  while (count--)
-    newTrain.addCage(false);
-
-  std::cout << "Длина поезда: " << newTrain.getTrainLength() << std::endl;
-  std::cout << "Число переходов: " << newTrain.getStepsCount() << std::endl;
-
   return 0;
 }
